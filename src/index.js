b8dcdc3662be559020f61450fac475086209e476
drifterz13 <mixmyxxe@gmail.com>
2020-12-20 17:10:06 +0700

    add game logic

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import {
   render,
   html,
   useState,
+  useEffect,
 } from "https://unpkg.com/htm/preact/standalone.module.js";
 
 import { icons } from "./icons/index.js";
@@ -40,17 +41,74 @@ const BoardLayout = () => {
       shuffledItems.push(item);
       itemCountMap[item] += 1;
     }
-    console.log("final", itemCountMap, shuffledItems);
     return shuffledItems;
   };
 
   const [items, setItems] = useState(initialState);
+  const [guessItems, setGuessItems] = useState([]);
+  const [showItems, setShowItems] = useState(new Set());
+  const [computing, setComputing] = useState(false);
+
+  const DELAY = 500;
+
+  useEffect(() => {
+    if (showItems.size === 16) {
+      setShowItems(new Set());
+      setItems(initialState);
+      console.log("You win!");
+    }
+  }, [showItems]);
+
+  useEffect(() => {
+    if (guessItems.length === 2) {
+      const item1 = items[guessItems[0]];
+      const item2 = items[guessItems[1]];
+      if (item1 === item2) {
+        console.log("matched");
+        setShowItems((state) => {
+          let newState = state;
+          newState.add(guessItems[0]);
+          newState.add(guessItems[1]);
+          return new Set(newState);
+        });
+      } else {
+        console.log("not matched");
+        setComputing(true);
+        setTimeout(() => {
+          setShowItems((state) => {
+            let newState = state;
+            newState.delete(guessItems[0]);
+            newState.delete(guessItems[1]);
+            return new Set(newState);
+          });
+          setComputing(false);
+        }, DELAY);
+      }
+      setGuessItems([]);
+      return;
+    }
+  }, [guessItems]);
+
+  const guess = (index) => {
+    if (computing) {
+      return;
+    }
+    if (showItems.has(index)) {
+      return;
+    }
+    setGuessItems((state) => [...state, index]);
+    setShowItems((state) => {
+      const newState = state;
+      newState.add(index);
+      return new Set(newState);
+    });
+  };
 
   return html`
     <div class="board-layout">
       ${items.map(
-        (item) => html` <${Box}>
-          <${icons[item]} />
+        (item, index) => html` <${Box} onClick=${() => guess(index)}>
+          ${showItems.has(index) && html` <${icons[item]} /> `}
         <//>`
       )}
     </div>
