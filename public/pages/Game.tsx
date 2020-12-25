e2ba6a444946730c35032e6f3d697b34601373e6
drifterz13 <mixmyxxe@gmail.com>
2020-12-25 21:41:43 +0700

    handle error

--- public/pages/Game.tsx
+++ public/pages/Game.tsx
@@ -1,12 +1,13 @@
 import { Fragment } from "preact";
 import { useEffect } from "preact/hooks";
 // @ts-ignore
-import { useMachine } from 'preact-robot'
+import { useMachine } from "preact-robot";
 import BoardLayout from "../components/BoardLayout";
 import StartSection from "../components/StartSection";
 import TimeTracking from "../components/TimeTracking";
 import Loading from "../components/Loading";
 import { EVENT, machine } from "../lib/gameMachine";
+import Modal from "../components/Modal";
 
 export default function Game() {
   const [current, send] = useMachine(machine);
@@ -38,6 +39,14 @@ export default function Game() {
         }
       />
       {current.name === "saving" ? <Loading /> : null}
+      {current.name === "failed" ? (
+        <Modal
+          heading="Error! 👻"
+          description="Something went wrong. Please try again."
+          onClose={() => send(EVENT.RESET)}
+          onCancel={() => send(EVENT.RESET)}
+        />
+      ) : null}
     </Fragment>
   );
 }
